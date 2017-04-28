dcb8fa5acafae7388745387618bec7915b30f2c3
galipaydin <gaydin@firat.edu.tr>
2017-04-28 17:29:46 +0300

    v0.1

--- src/main/java/org/buyukveri/iett/OtobusHatlari.java
+++ src/main/java/org/buyukveri/iett/OtobusHatlari.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.buyukveri.ibb;
+package org.buyukveri.iett;
 
 import java.io.BufferedInputStream;
 import java.io.File;
